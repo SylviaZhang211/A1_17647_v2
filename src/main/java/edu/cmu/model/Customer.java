0e314f9c7d32e68cabbea96e518eafabe2813a48
SylviaZhang211 <sylviazhang211@gmail.com>
2023-04-05 20:35:40 -0400

    update

--- src/main/java/edu/cmu/model/Customer.java
+++ src/main/java/edu/cmu/model/Customer.java
@@ -12,12 +12,7 @@ import java.util.Set;
 @Entity
 @Data
 public class Customer {
-    static Set<String> validStateAbbreviations = Set.of(
-            "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID",
-            "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT",
-            "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-            "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
-    );
+
     @Id
     //@NonNull
     @JsonProperty("id")
@@ -65,8 +60,5 @@ public class Customer {
     @JsonProperty("zipcode")
     private String zipcode;
 
-    @JsonIgnore
-    public boolean isStateInvalid() {
-        return !validStateAbbreviations.contains(state);
-    }
+
 }
