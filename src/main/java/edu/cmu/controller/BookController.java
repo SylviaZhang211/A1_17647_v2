1653594470787c66419224318587d64bf3789127
SylviaZhang211 <sylviazhang211@gmail.com>
2023-04-05 20:29:45 -0400

    update

--- src/main/java/edu/cmu/controller/BookController.java
+++ src/main/java/edu/cmu/controller/BookController.java
@@ -2,17 +2,13 @@ package edu.cmu.controller;
 
 import edu.cmu.model.Book;
 import edu.cmu.model.Customer;
-import edu.cmu.response.ErrorMessage;
 import edu.cmu.service.BookService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import edu.cmu.utils.Validation;
 import jakarta.validation.Valid;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,64 +16,125 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     final
-    BookService bookService;
+    BookService bookService ;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
-       //bookService.deleteAll();
     }
 
-    @Operation(summary = "Add a new book")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Successfully added a new book",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
-            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content),
-            @ApiResponse(responseCode = "422", description = "ISBN already exists in the system", content = @Content)
-    })
     @PostMapping("")
-    public ResponseEntity<?> addBook(@Valid @RequestBody Book book) {
-        if (book.isPriceInvalid()) {
-            return new ResponseEntity<>(new ErrorMessage("The price of the book is not valid"), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> addBook(@Valid @RequestBody Book book, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("Missing information"), HttpStatus.BAD_REQUEST);
+        } else if(bookService.findBook(book.getISBN()) != null){
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("This ISBN already exists in the system."), HttpStatus.UNPROCESSABLE_ENTITY);
+        }else if (Validation.checkPrice(book.getPrice())) {
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("Malformed input: price"), HttpStatus.BAD_REQUEST);
         }
+        //response.setHeader("Location", request.getRequestURL().append("/").append(createdHotel.getId()).toString());
         return new ResponseEntity<>(bookService.addBook(book), HttpStatus.CREATED);
     }
 
-    @Operation(summary = "update a book by ISBN")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved the book",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) }),
-            @ApiResponse(responseCode = "404", description = "Book Not Found", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
-    })
-    @PutMapping("/{ISBN}")
-    public ResponseEntity<?> updateBook(@Valid @RequestBody Book book, @PathVariable String ISBN) {
-        if (book.isPriceInvalid()) {
-            return new ResponseEntity<>(new ErrorMessage("The price of the book is not valid"), HttpStatus.BAD_REQUEST);
+    @PutMapping("{ISBN}")
+    public ResponseEntity<?> updateBook(@PathVariable String ISBN, @Valid @RequestBody Book book, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("Missing information"), HttpStatus.BAD_REQUEST);
+        } else if(bookService.findBook(book.getISBN()) == null){
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("ISBN not found"), HttpStatus.NOT_FOUND);
+        }else if (Validation.checkPrice(book.getPrice())) {
+            return new ResponseEntity<>(new ErrorMessage("Malformed input: price"), HttpStatus.BAD_REQUEST);
         }
-        if (bookService.findBook(ISBN) == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        //response.setHeader("Location", request.getRequestURL().append("/").append(createdHotel.getId()).toString());
         return new ResponseEntity<>(bookService.updateBook(book), HttpStatus.OK);
 
     }
 
-    @Operation(summary = "retrieve a book by ISBN")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved the book",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) }),
-            @ApiResponse(responseCode = "404", description = "Book Not Found", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
-    })
-    @GetMapping({"/{ISBN}", "/isbn/{ISBN}"})
-    public ResponseEntity<?> retrieveBook(@PathVariable String ISBN) {
+    @GetMapping({"isbn/{ISBN}","{ISBN}"})
+    public ResponseEntity<?> getBook(@PathVariable String ISBN){
         var book = bookService.findBook(ISBN);
-        return book == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(book, HttpStatus.OK);
+        if (book != null){
+
+            return new ResponseEntity<>(book, HttpStatus.OK);
+        } else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> validationError(MethodArgumentNotValidException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
 }
+
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.media.Content;
+//import io.swagger.v3.oas.annotations.media.Schema;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import jakarta.validation.Valid;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/books")
+//public class BookController {
+//
+//    final
+//    BookService bookService;
+//
+//    public BookController(BookService bookService) {
+//        this.bookService = bookService;
+//       //bookService.deleteAll();
+//    }
+//
+//    @Operation(summary = "Add a new book")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "201", description = "Successfully added a new book",
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
+//            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content),
+//            @ApiResponse(responseCode = "422", description = "ISBN already exists in the system", content = @Content)
+//    })
+//    @PostMapping("")
+//    public ResponseEntity<?> addBook(@Valid @RequestBody Book book) {
+//        if (book.isPriceInvalid()) {
+//            return new ResponseEntity<>(new ErrorMessage("The price of the book is not valid"), HttpStatus.BAD_REQUEST);
+//        }
+//        return new ResponseEntity<>(bookService.addBook(book), HttpStatus.CREATED);
+//    }
+//
+//    @Operation(summary = "update a book by ISBN")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Successfully retrieved the book",
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) }),
+//            @ApiResponse(responseCode = "404", description = "Book Not Found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
+//    })
+//    @PutMapping("/{ISBN}")
+//    public ResponseEntity<?> updateBook(@Valid @RequestBody Book book, @PathVariable String ISBN) {
+//        if (book.isPriceInvalid()) {
+//            return new ResponseEntity<>(new ErrorMessage("The price of the book is not valid"), HttpStatus.BAD_REQUEST);
+//        }
+//        if (bookService.findBook(ISBN) == null) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<>(bookService.updateBook(book), HttpStatus.OK);
+//
+//    }
+//
+//    @Operation(summary = "retrieve a book by ISBN")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Successfully retrieved the book",
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Book.class)) }),
+//            @ApiResponse(responseCode = "404", description = "Book Not Found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
+//    })
+//    @GetMapping({"/{ISBN}", "/isbn/{ISBN}"})
+//    public ResponseEntity<?> retrieveBook(@PathVariable String ISBN) {
+//        var book = bookService.findBook(ISBN);
+//        return book == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(book, HttpStatus.OK);
+//    }
+//
+//
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<?> validationError(MethodArgumentNotValidException ex) {
+//        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//    }
+//
+//}
--- src/main/java/edu/cmu/controller/CustomerController.java
+++ src/main/java/edu/cmu/controller/CustomerController.java
@@ -1,79 +1,142 @@
 package edu.cmu.controller;
 
 import edu.cmu.model.Customer;
-import edu.cmu.response.ErrorMessage;
 import edu.cmu.service.CustomerService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.ConstraintViolationException;
+import edu.cmu.utils.Validation;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-@Validated
+@Slf4j
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-
     final
     CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
-        //customerService.deleteAll();
     }
 
-    @Operation(summary = "Add a new customer")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Successfully added a new customer",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
-            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content),
-            @ApiResponse(responseCode = "422", description = "User-ID already exists in the system", content = @Content)
-    })
+    //@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping("")
-    public ResponseEntity<?> addCustomer(@Valid @RequestBody Customer customer) {
-        if (customer.isStateInvalid()) {
-            return new ResponseEntity<>(new ErrorMessage("The state of the customer is not valid"), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> addCustomer(@Valid @RequestBody Customer customer, BindingResult bindingResult) {
+        log.info(customer.toString());
+        log.info(bindingResult.getAllErrors().toString());
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("Missing information"), HttpStatus.BAD_REQUEST);
+        } else if (!Validation.checkState(customer.getState())) {
+            return new ResponseEntity<>(new org.springdoc.api.ErrorMessage("Malformed input: state"), HttpStatus.BAD_REQUEST);
+        } else if (customerService.findCustomerByEmail(customer.getUserId()) != null) {
+            return new ResponseEntity<>(new ErrorMessage("This user ID already exists in the system."), HttpStatus.UNPROCESSABLE_ENTITY);
         }
+
         return new ResponseEntity<>(customerService.addCustomer(customer), HttpStatus.CREATED);
     }
 
-    @Operation(summary = "retrieve a customer by ID")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
-            @ApiResponse(responseCode = "404", description = "Customer Not Found", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
-    })
-    @GetMapping("/{id}")
-    public ResponseEntity<?> retrieveCustomerById(@PathVariable int id) {
+
+    @GetMapping({"/{id}"})
+    public ResponseEntity<?> getCustomerById(@PathVariable int id) {
         var customer = customerService.findCustomerByID(id);
-        return customer == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(customer, HttpStatus.OK);
+        if (customer != null) {
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
-    @Operation(summary = "retrieve a customer by userId")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer",
-                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
-            @ApiResponse(responseCode = "404", description = "Customer Not Found", content = @Content),
-            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
-    })
     @GetMapping("")
-    public ResponseEntity<?> retrieveCustomerByUserId(@RequestParam @Email String userId) {
+    public ResponseEntity<?> getCustomerByEmail(@RequestParam("userId") String userId) {
+        userId = userId.replace("%40", "@");
         var customer = customerService.findCustomerByEmail(userId);
-        return customer == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(customer, HttpStatus.OK);
-    }
+        if (!Validation.checkEmail(userId)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        if (customer != null) {
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
-    public ResponseEntity<?> validationError(Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
-
 }
+
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.media.Content;
+//import io.swagger.v3.oas.annotations.media.Schema;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import jakarta.validation.ConstraintViolationException;
+//import jakarta.validation.Valid;
+//import jakarta.validation.constraints.Email;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.annotation.Validated;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.*;
+//
+//@Validated
+//@RestController
+//@RequestMapping("/customers")
+//public class CustomerController {
+//
+//    final
+//    CustomerService customerService;
+//
+//    public CustomerController(CustomerService customerService) {
+//        this.customerService = customerService;
+//        //customerService.deleteAll();
+//    }
+//
+//    @Operation(summary = "Add a new customer")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "201", description = "Successfully added a new customer",
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
+//            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content),
+//            @ApiResponse(responseCode = "422", description = "User-ID already exists in the system", content = @Content)
+//    })
+//    @PostMapping("")
+//    public ResponseEntity<?> addCustomer(@Valid @RequestBody Customer customer) {
+//        if (customer.isStateInvalid()) {
+//            return new ResponseEntity<>(new ErrorMessage("The state of the customer is not valid"), HttpStatus.BAD_REQUEST);
+//        }
+//        return new ResponseEntity<>(customerService.addCustomer(customer), HttpStatus.CREATED);
+//    }
+//
+//    @Operation(summary = "retrieve a customer by ID")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer",
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
+//            @ApiResponse(responseCode = "404", description = "Customer Not Found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
+//    })
+//    @GetMapping("/{id}")
+//    public ResponseEntity<?> retrieveCustomerById(@PathVariable int id) {
+//        var customer = customerService.findCustomerByID(id);
+//        return customer == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(customer, HttpStatus.OK);
+//    }
+//
+//    @Operation(summary = "retrieve a customer by userId")
+//    @ApiResponses(value = {
+//            @ApiResponse(responseCode = "200", description = "Successfully retrieved the customer",
+//                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
+//            @ApiResponse(responseCode = "404", description = "Customer Not Found", content = @Content),
+//            @ApiResponse(responseCode = "400", description = "Illegal, missing, or malformed input", content = @Content)
+//    })
+//    @GetMapping("")
+//    public ResponseEntity<?> retrieveCustomerByUserId(@RequestParam @Email String userId) {
+//        var customer = customerService.findCustomerByEmail(userId);
+//        return customer == null ? new ResponseEntity<>(HttpStatus.NOT_FOUND) : new ResponseEntity<>(customer, HttpStatus.OK);
+//    }
+//
+//    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+//    public ResponseEntity<?> validationError(Exception ex) {
+//        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+//    }
+//
+//}
